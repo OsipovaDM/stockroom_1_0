32e8297c8a033026a96966483680842dd454a00f
Дарья Осипова <OsipovaDM@yandex.ru>
2025-09-14 16:58:30 +0300

    Регистрация новых пользователей

--- stockroom/stockroom/urls.py
+++ stockroom/stockroom/urls.py
@@ -1,8 +1,19 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic.edit import CreateView
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, reverse_lazy
 
 urlpatterns = [
     path('', include('data.urls')),
     path('admin/', admin.site.urls),
+    path(
+        'auth/registration/',
+        CreateView.as_view(
+            template_name='registration/registration_form.html',
+            form_class=UserCreationForm,
+            success_url=reverse_lazy('data:cells'),
+        ),
+        name='registration',
+    ),
     path('auth/', include('django.contrib.auth.urls')),
 ]
